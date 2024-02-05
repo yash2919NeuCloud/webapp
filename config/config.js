6e9cbf1812d6c8954454f8d81894de44bb0eeaa3
yash2919 <nahata.y@northeastern.edu>
2024-02-04 20:27:24 -0500

    comments removed

--- config/config.js
+++ config/config.js
@@ -1,7 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+
+try {
+   sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
     host: process.env.DB_HOST,
   });
+} catch (error) {
+
+}
+
   module.exports = { sequelize };

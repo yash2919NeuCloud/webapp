e8315d22703ef61bdb5391cbfafe10f595822d70
yash2919 <nahata.y@northeastern.edu>
2024-02-06 18:56:31 -0500

    Validations Added

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.use((err, req, res, next) => {
 
 app.use('/healthz', healthzRouter);
 app.use('/v1/user', userRouter);
-
+app.use('/', (req, res) => {
+  res.status(404).send();
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });

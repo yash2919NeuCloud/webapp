7d18bbd02bd14f66af9a82f2cff64607d368a9ae
yash2919 <nahata.y@northeastern.edu>
2024-02-06 21:59:31 -0500

    Fixed Conflicts

--- config/config.js
+++ config/config.js
@@ -1,13 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-
-try {
-   sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
     host: process.env.DB_HOST,
   });
-} catch (error) {
-
-}
-
   module.exports = { sequelize };

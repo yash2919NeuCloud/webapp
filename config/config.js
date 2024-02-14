667bdad8bc3a7694a702c93e56d231301c8ff261
yash2919 <nahata.y@northeastern.edu>
2024-02-13 19:36:51 -0500

    test change

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 
 try {
   console.log('DB_DATABASE', process.env.DB_DATABASE)
-   sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+   sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER || 'root', process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'localhost',
   });
 } catch (error) {
 

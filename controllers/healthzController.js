4c3a8220451a273a50e86f8ba37768c1c2cb9e33
yash2919 <nahata.y@northeastern.edu>
2024-03-18 02:31:06 -0400

    upd

--- controllers/healthzController.js
+++ controllers/healthzController.js
@@ -1,5 +1,21 @@
 const healthzService = require('../services/healthzService');
-const logger = require('../server').logger;
+const winston = require('winston');
+const { LoggingWinston } = require('@google-cloud/logging-winston');
+const logFilePath = '/var/log/webapp.log';
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+    new LoggingWinston(),
+    new winston.transports.File({ filename: logFilePath })
+  ],
+  format: winston.format.combine(
+    winston.format.timestamp(), // Add timestamp to logs
+    winston.format.errors({ stack: true }), // Include stack traces for errors
+    winston.format.json() // Output logs in JSON format
+  )
+});
+
 async function healthz(req, res) {
   if (Object.keys(req.body).length > 0) {
     res.status(400).header('Cache-Control', 'no-cache').send();
@@ -9,7 +25,7 @@ async function healthz(req, res) {
         const isDatabaseConnected = await healthzService.checkDatabaseConnection();
         console.log('isDatabaseConnected:', isDatabaseConnected);
         if (isDatabaseConnected) {
-          logger.info({message: 'Database is connected'});
+         logger.info({message: 'Database is connected'});
                   res.status(200).header('Cache-Control', 'no-cache').send();
                 } else {
                   logger.error({message: 'Database is not connected'});
@@ -17,7 +33,7 @@ async function healthz(req, res) {
                   res.status(503).header('Cache-Control', 'no-cache').send();
                 }
               } catch (error) {
-                logger.error({message: 'Database is not connected', additionalData: { subsystem: error } });
+                logger.error({message: 'Database is not connected'});
                // console.error('Error checking database connection:',error);
                 res.status(503).header('Cache-Control', 'no-cache').send();
               }

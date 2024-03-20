ec6ece1bf508d9775bd70ae74193f34adee0ce06
yash2919 <nahata.y@northeastern.edu>
2024-03-19 21:04:33 -0400

    upd

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
-
+const winston = require('winston');
+const { LoggingWinston } = require('@google-cloud/logging-winston');
+const logFilePath = '/var/log/webapp/app.log';
 
 try {
   console.log('DB_DATABASE', process.env.DB_DATABASE)
@@ -13,4 +15,18 @@ try {
 
 }
 
-  module.exports = { sequelize };
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+    new LoggingWinston(),
+    new winston.transports.File({ filename: logFilePath })
+  ],
+  format: winston.format.combine(
+    winston.format.timestamp(), // Add timestamp to logs
+    winston.format.errors({ stack: true }), // Include stack traces for errors
+    winston.format.json() // Output logs in JSON format
+  )
+});
+
+  module.exports = { sequelize,logger };

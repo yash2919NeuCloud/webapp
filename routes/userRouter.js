8a029ec526c25a5295775bbc0c66d9b8e745debe
yash2919 <nahata.y@northeastern.edu>
2024-03-26 22:56:30 -0400

    added verify email code

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,8 +2,10 @@ const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController.js');
 
+userRouter.get('/verify', userController.verifyUser);
 userRouter.get('/self', userController.getUser);
 userRouter.put('/self', userController.updateUser);
+
 userRouter.post('/', userController.createUser);
 
 module.exports = userRouter;
